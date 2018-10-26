593dd7bae7b89918bed4dc978928bdb0390b793c
Rubberbandman2002 <lucastinouye@gmail.com>
2018-10-26 14:09:40 -0700

    done

--- app/src/main/java/com/example/bruce/triples_1_5/Card.java
+++ app/src/main/java/com/example/bruce/triples_1_5/Card.java
@@ -1,6 +1,60 @@
 package com.example.bruce.triples_1_5;
 
 public class Card {
+    private Shape mShape;
+    private Color mColor;
+    private int  mNum;
+    private int mAlpha;
+    private boolean mIsSelected = false;
+
+    /*****************************************************
+     * Enum
+     ****************************************************/
+    protected enum Shape {
+        CIRCLE, SQUARE, TRIANGLE, NO_SHAPE
+    }
+
+    protected enum Color{
+        RED, BLUE, YELLOW, NO_COLOR
+    }
+
+    public Card( Shape shape, Color color, int alpha, int num, boolean isSelected) {
+        mShape = shape;
+        mColor = color;
+        mAlpha = alpha;
+        mNum = num;
+        mIsSelected = isSelected;
 }
+
+    /******************************************************
+     *Getters and Setters
+    ******************************************************/
+
+    public Shape getShape(){
+        return mShape;
+    }
+
+    public Color getColor(){
+        return mColor;
+    }
+
+    public int getNum(){
+        return mNum;
+    }
+
+    public int getAlpha() {
+        return mAlpha;
+    }
+
+    public void setIsSelected(boolean isSelected) {
+       mIsSelected = isSelected;
+    }
+
+    public boolean getIsSeleceted() {
+        return mIsSelected;
+    }
+
+}
+
 
 
---
+++ app/src/test/java/com/example/bruce/triples_1_5/CardTest.java
@@ -0,0 +1,60 @@
+package com.example.bruce.triples_1_5;
+import android.support.v7.app.AppCompatActivity;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CardTest {
+    Card c;
+    @Test
+    public void getShape() {
+        Card.Shape actualValue;
+        Card.Shape expectedValue[] ={Card.Shape.CIRCLE, Card.Shape.SQUARE, Card.Shape.TRIANGLE, Card.Shape.NO_SHAPE};
+        for(int i = 0; i < 4; i++) {
+        c = new Card (Card.Shape.values()[i], Card.Color.NO_COLOR,-1, -1, false );
+            actualValue = c.getShape();
+            assertTrue(actualValue == expectedValue[i]);
+        }
+    }
+
+    @Test
+    public void getColor() {
+        Card.Color actualValue;
+        Card.Color expectedValue[] = {Card.Color.RED, Card.Color.BLUE, Card.Color.YELLOW, Card.Color.NO_COLOR};
+        for(int i = 0; i < 4; i++) {
+            c = new Card (Card.Shape.NO_SHAPE, Card.Color.values()[i], -1, -1, false);
+            actualValue = c.getColor();
+            assertTrue(actualValue == expectedValue[i]);}
+    }
+
+    @Test
+    public void getNum() {
+        int[] expectedValue = {0, 1, 2};
+        int actualValue = -1;
+        for(int i = 0; i < 3; i++) {
+            c = new Card (Card.Shape.NO_SHAPE, Card.Color.NO_COLOR, -1, expectedValue[i], false);
+            actualValue = c.getNum();
+            assertTrue(actualValue == expectedValue[i]);
+        }
+    }
+
+    @Test
+    public void getAlpha() {
+    int[] expectedValue = {0, 1, 2};
+    int actualValue = -1;
+    for(int i = 0; i < 3; i++){
+        c = new Card (Card.Shape.NO_SHAPE, Card.Color.NO_COLOR, expectedValue[i], -1, false );
+        actualValue = c.getAlpha();
+        assertTrue(actualValue == expectedValue[i]);
+    }
+    }
+
+    @Test
+    public void setIsSelected() {
+        c = new Card (Card.Shape.NO_SHAPE, Card.Color.NO_COLOR, -1, -1, false );
+       boolean expectedValue =  true;
+       c.setIsSelected(expectedValue);
+       boolean  actualValue = c.getIsSeleceted();
+       assertTrue(actualValue);
+    }
+    }
